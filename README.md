c3e0f00831ec92105b773ed5e7ac8e8f77a1e050
Puneet Sharma <puneetpremsharma@hotmail.com>
2023-08-12 22:20:24 +0530

    added the decrement in the redux to explain the working with types

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
-# Redux-simple-demo
+# Redux Simple Demo
+
+This is a simple demo for the redux code. Only one file is created to explain the working of redux in JavaScript. Please have a look at the comment where I have metioned the working of the functions. 
+
+## After download/Clone the project
+
+Please do following steps 
+
+- To install the redux package run
+
+    `npm install` 
+
+- To run the project you can run
+
+    `npm start`
+
+Now you can see the followig result in the command line
+
+    First State :- { counter: 0 }
+    latestState :- { counter: 1 }
+    latestState :- { counter: 2 }
+    latestState :- { counter: 1 }
--- index-redux.js
+++ index-redux.js
@@ -1,16 +1,39 @@
 const redux = require("redux");
+
 const counterReducer = (state = {counter:0}, action) => {
-    return {
-        counter:state.counter + 1
-    };
+    if (action.type === 'INCREMENT') {
+        return {
+            counter:state.counter + 1
+        };
+    } else if (action.type === 'DECREMENT') {
+        return {
+            counter:state.counter - 1
+        };
+    } else {
+        return {
+            counter:state.counter
+        };
+    }
+    
 };
 const store = redux.createStore(counterReducer);
+
+const firstState = store.getState();
+console.log("First State :-", firstState)
 
 const counterSubscriber = () =>{
     const latestState = store.getState();
     console.log("latestState :-", latestState)
 }
-
+// Adds a change listener. It will be called any time an action is dispatched,
 store.subscribe(counterSubscriber);
 
+// with this increase one counter current = 1 
 store.dispatch({type:'INCREMENT'});
+
+// with this increase one counter current = 2
+store.dispatch({type:'INCREMENT'});
+
+
+// with this increase one counter current = 3
+store.dispatch({type:'DECREMENT'});
